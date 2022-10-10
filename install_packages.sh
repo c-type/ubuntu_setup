91bc8cc1719e5bfc9bd92101f96252fc1c98adc0
Clemens Rumpf <clemens@astranis.com>
2022-10-10 12:57:28 -0700

    Install sublime merge

--- install_packages.sh
+++ install_packages.sh
@@ -28,3 +28,11 @@ sudo apt install flake8
 
 # Install ripgrep
 sudo apt-get install ripgrep
+
+# Install Sublime-Merge
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-merge
+
