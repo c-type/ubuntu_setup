d03a373c5fc28a5ed7683abbd29a877d09e35063
Clemens Rumpf <ctypecodes@gmail.com>
2022-10-29 09:58:12 -0700

    auto confirm install and few new packages

--- install_packages.sh
+++ install_packages.sh
@@ -2,50 +2,56 @@
 
 # Install NeoVim
 ## Install NeoVim itself
-sudo apt-get install neovim
-sudo apt-get install software-properties-common
+sudo apt-get install neovim -y
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install neovim -y
 
 ## Install pre-configured setup
-git clone https://github.com/LunarVim/Neovim-from-scratch.git ~/.config/nvim
+git clone git@github.com:c-type/nvim_setup.git ~/.config/nvim
 
 ## Enable copy/paste
-sudo apt install xsel
+sudo apt install xsel -y
 
 ## Install Python support
 pip install pynvim
 
 ## Install NodeJS
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install nodejs -y
+sudo apt install npm -y
 npm i -g neovim
 
 # Install code formatting and linting
-sudo apt install black
-sudo apt install flake8
+sudo apt install black -y
+sudo apt install flake8 -y
 
 # Install ripgrep
-sudo apt-get install ripgrep
+sudo apt-get install ripgrep -y
 
 # Install Sublime-Merge
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-merge
+sudo apt-get install sublime-merge -y
 
-# Install Docker
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# # Install Docker
+# sudo apt-get install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install terminator
+sudo apt-get install terminator -y
+
+# Install KeePassXC
+sudo apt install keepassxc -y
